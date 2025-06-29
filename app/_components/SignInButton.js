e523a639c628909dd6699a73bbd8508af98f7e82
Youssef Hikal <youssefhikal@gmail.com>
2025-06-29 21:10:32 +0200

    facebook works only for testing, cant push it into production due to bussines verfication

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -20,23 +20,23 @@ function SignInButton() {
         <span>Continue with Google</span>
       </button>
 
-      <button
-        type="submit"
-        name="provider"
-        value="facebook"
-        className="flex items-center gap-6 text-md border  hover:text-primary-950 border-primary-300 px-10 py-4 font-medium hover:bg-gray-50 transition-colors w-full justify-center"
-      >
-        <div className="relative">
-          <Image
-            src="https://authjs.dev/img/providers/facebook.svg"
-            alt="Facebook logo"
-            height="24"
-            width="24"
-          />
-        </div>
+      // <button
+      //   type="submit"
+      //   name="provider"
+      //   value="facebook"
+      //   className="flex items-center gap-6 text-md border  hover:text-primary-950 border-primary-300 px-10 py-4 font-medium hover:bg-gray-50 transition-colors w-full justify-center"
+      // >
+      //   <div className="relative">
+      //     <Image
+      //       src="https://authjs.dev/img/providers/facebook.svg"
+      //       alt="Facebook logo"
+      //       height="24"
+      //       width="24"
+      //     />
+      //   </div>
 
-        <span>Continue with Facebook</span>
-      </button>
+      //   <span>Continue with Facebook</span>
+      // </button>
     </div>
   );
 }
